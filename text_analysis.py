78ef7ac8469ea7cf726106ef13c8bcb91ef8706d
sahil-s-246 <sahilsrinivas3@gmail.com>
2022-05-13 21:28:02 +0530

    data

--- text_analysis.py
+++ text_analysis.py
@@ -1,16 +1,25 @@
 from textblob import TextBlob
-from newspaper import Article
-import nltk
 
-url = 'https://www.wionews.com/world/finland-likely-to-join-nato-this-summer-minister-lintila-to-wion-471933'
-article = Article(url)
 
-article.download()
-article.parse()
-article.nlp()
+def outcome(polarity):
+    """Returns the sentiment [positive/negative or neutral] based on polarity"""
+    if polarity > 0:
+        return "Positive"
+    elif polarity == 0:
+        return "Neutral"
+    else:
+        return "Negative"
 
-text = article.summary
-print(text)
-blob = TextBlob(text)
-sentiment = blob.sentiment.polarity
-print(sentiment)
+
+with open("sample_data.txt", encoding="utf-8") as sample_file:
+    data = sample_file.readlines()
+    for line in data:
+        print(line, end=" ")
+        text = TextBlob(line)
+        # A general text block, meant for larger bodies of text (esp. those containing sentences). Inherits from
+        # BaseBlob  .
+        sentiment = text.sentiment.polarity
+        # Return a tuple of form (polarity, subjectivity ) where polarity is a float within the range [-1.0,
+        # 1.0] and subjectivity is a float within the range [0.0, 1.0] where 0.0 is very objective and 1.0 is very
+        # subjective.
+        print(f'{outcome(sentiment)} \n')
