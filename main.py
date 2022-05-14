5179495935108c296454832b6eb83886a58d4898
Yash Jawale <yash25.j@gmail.com>
2022-05-14 13:00:57 +0530

    Moved to main.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from textblob import TextBlob
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def outcome(polarity):
+    """Returns the sentiment [positive/negative or neutral] based on polarity"""
+    if polarity > 0:
+        return "Positive"
+    elif polarity == 0:
+        return "Neutral"
+    else:
+        return "Negative"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with open("sample_data.txt", encoding="utf-8") as sample_file:
+    data = sample_file.readlines()
+    for line in data:
+        print(line, end=" ")
+        text = TextBlob(line)
+        # A general text block, meant for larger bodies of text (esp. those containing sentences). Inherits from
+        # BaseBlob  .
+        sentiment = text.sentiment.polarity
+        # Return a tuple of form (polarity, subjectivity ) where polarity is a float within the range [-1.0,
+        # 1.0] and subjectivity is a float within the range [0.0, 1.0] where 0.0 is very objective and 1.0 is very
+        # subjective.
+        print(f'{outcome(sentiment)} \n')
